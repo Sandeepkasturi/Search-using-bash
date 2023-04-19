055231ebf7a7c5504961cc55e37aac5be99785a5
Sandeepkasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2023-04-19 22:05:44 +0530

    code

--- sample.sh
+++ sample.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Creating sandeep logo..."
+echo " SSSSS    AAAAA   N     N   DDDDD    EEEEEEE   EEEEEEE   PPPPPP "
+echo "SS        AA  AA  NN    N   DD   DD  EE        EE        PP   PP"
+echo " SSSSS    AAAAAA  N N   N   DD    DD EEEEE     EEEEE     PPPPPP "
+echo "     SS   AA  AA  N  N  N   DD   DD  EE        EE        PP     "
+echo "SSSSSS    AA  AA  N   N N   DDDDDD   EEEEEEE   EEEEEEE   PP     "
+echo "Logo created successfully!"
 search_term="sandeepkasturi.blogspot.com"
 url="https://www.google.com/search?q=${search_term}"
 xdg-open $url
